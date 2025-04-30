37ed1a3d67baea0055fffe174a59c3bef36c0ae1
myeonghan-nim <hannim0828@gmail.com>
2025-04-30 20:59:17 +0900

    Add: web server entrypoint

---
+++ app/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+: "${DJANGO_SETTINGS_MODULE:=storyum.settings}"
+
+echo ">>> Migration"
+python manage.py migrate --noinput
+
+# TODO: static files if needed
+# echo ">>> Collect static files"
+# python manage.py collectstatic --noinput
+
+echo ">>> Starting server"
+exec gunicorn storyum.wsgi:application --config ./gunicorn.py
