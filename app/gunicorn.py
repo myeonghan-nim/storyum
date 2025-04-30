531302a9febe44cf65e61756dbc3009363a5d7c7
myeonghan-nim <hannim0828@gmail.com>
2025-04-30 20:59:00 +0900

    Add: gunicorn configuration

---
+++ app/gunicorn.py
@@ -0,0 +1,21 @@
+import os
+import multiprocessing
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bind = "0.0.0.0:8000"
+
+workers = int(os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+max_requests = int(os.getenv("GUNICORN_MAX_REQUESTS", 1024))
+max_requests_jitter = 256
+
+timeout = int(os.getenv("GUNICORN_TIMEOUT", 120))
+
+keepalive = int(os.getenv("GUNICORN_KEEPALIVE", 5))
+
+# TODO: logging
+# accesslog = "-"
+# errorlog = "-"
+# loglevel = os.getenv("GUNICORN_LOG_LEVEL", "info")
