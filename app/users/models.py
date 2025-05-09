fdbf45705491e8040a90d015552ff5defb3e66ca
myeonghan-nim <hannim0828@gmail.com>
2025-05-09 22:17:59 +0900

    Change: user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
