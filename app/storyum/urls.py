6a003a70f20cf18a06e92a105b82952e82bf3442
myeonghan-nim <hannim0828@gmail.com>
2025-05-09 22:17:18 +0900

    Change: url naming

--- app/storyum/urls.py
+++ app/storyum/urls.py
@@ -22,7 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path("admin/", admin.site.urls),
     # 사용자
-    path("api/v1/auth/", include("users.urls")),
+    path("api/v1/users/", include("users.urls")),
     # 문서
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
