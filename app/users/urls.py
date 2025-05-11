61ac7f167502006ace5b0bde91c6946606c72c5f
myeonghan-nim <hannim0828@gmail.com>
2025-05-11 23:10:00 +0900

    Add: logout and token refresh URL

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
     UserLoginViewSet,
+    UserLogoutViewSet,
     UserRegistrationViewSet,
 )
 
 router = DefaultRouter()
 router.register(r"register", UserRegistrationViewSet, basename="users-register")
 router.register(r"login", UserLoginViewSet, basename="users-login")
+router.register(r"logout", UserLogoutViewSet, basename="users-logout")
 
 urlpatterns = [
     path("", include(router.urls)),
+    # 3rd-party JWT views
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
