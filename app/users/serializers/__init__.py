58469c1b64bd603443c39e4dff39324d7a21438e
myeonghan-nim <hannim0828@gmail.com>
2025-05-11 23:09:27 +0900

    Add: logout serializer

--- app/users/serializers/__init__.py
+++ app/users/serializers/__init__.py
@@ -1,7 +1,9 @@
 from .login import UserLoginSerializer
+from .logout import UserLogoutSerializer
 from .registration import UserRegistrationSerializer
 
 __all__ = [
     "UserLoginSerializer",
+    "UserLogoutSerializer",
     "UserRegistrationSerializer",
 ]
