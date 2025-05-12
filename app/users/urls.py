a57b35e4f913984632da128bdd3ab5e17b624a75
myeonghan-nim <hannim0828@gmail.com>
2025-05-12 19:28:40 +0900

    Add: URL

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,12 +6,14 @@ from .views import (
     UserLoginViewSet,
     UserLogoutViewSet,
     UserRegistrationViewSet,
+    OTPRegisterViewSet,
 )
 
 router = DefaultRouter()
 router.register(r"register", UserRegistrationViewSet, basename="users-register")
 router.register(r"login", UserLoginViewSet, basename="users-login")
 router.register(r"logout", UserLogoutViewSet, basename="users-logout")
+router.register(r"otp/register", OTPRegisterViewSet, basename="users-otp-register")
 
 urlpatterns = [
     path("", include(router.urls)),
