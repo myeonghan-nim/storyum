dee0b752b4753b66d4aac5c062c29821d7b7bb1d
myeonghan-nim <hannim0828@gmail.com>
2025-05-12 19:28:05 +0900

    Add: TOTP in user

--- app/users/models.py
+++ app/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser, UserManager
 class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
+    otp_secret = models.CharField(max_length=255, blank=True, null=True)
+    otp_enabled = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
